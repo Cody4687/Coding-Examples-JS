144a6f1718326a990cf1003da09fc0dd81fc8bbd
Cody <38409768+Cody4687@users.noreply.github.com>
2019-03-08 13:56:14 -0600

    Create RussianRoulette.js
    A simple Russian Roulette function.

---
+++ RussianRoulette.js
@@ -0,0 +1,14 @@
+function RussianRoulette() {
+let math = Math.floor(Math.random() * 7)
+if (math < 1) {
+return('You died!')
+} else {
+return('You lived!')
+}
+}
+
+/*Example
+alert(RussianRoulette())
+or 
+console.log(RussianRoulette())
+*/
